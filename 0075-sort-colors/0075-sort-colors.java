0d48edc5205cbca67fe7983185cb431a6d9dbcab
ssrithik <166838233+ssrithik@users.noreply.github.com>
2024-09-18 09:38:49 +0530

    Time: 1 ms (24.44%), Space: 42.2 MB (10.05%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,46 +1,55 @@
 class Solution {
     public void sortColors(int[] a) {
-        //  HashMap<Integer,Integer> s = new HashMap<>();
-        // s.put(0,0);
-        // s.put(1,0);
-        // s.put(2,0);
-        // for(int n: nums)
-        // {
-        //     s.put
-
-        // }
-        int middle = 0;
-        int low=0;
-        int high = a.length-1;
-        int temp;
-        int i;
-       
-        while(middle <=high)
-        {
-           
-            if(a[middle] == 0)
-            {
-                   temp = a[middle];
-                   a[middle] = a[low];
-                   a[low] = temp;
-                   low++;
-                   middle++;
-            }
-            else if(a[middle] == 1)
-            {
-                middle++;
-            }
-            else if(a[middle]==2)
-            {
-                temp = a[middle];
-                a[middle] = a[high];
-                a[high] = temp;
-                high--;
-            }
-            
-            
-        }
         
+        // int middle = 0;
+        // int low=0;
+        // int high = a.length-1;
+        // int temp;
+        // int i;
+       
+        // while(middle <=high)
+        // {
+           
+        //     if(a[middle] == 0)
+        //     {
+        //            temp = a[middle];
+        //            a[middle] = a[low];
+        //            a[low] = temp;
+        //            low++;
+        //            middle++;
+        //     }
+        //     else if(a[middle] == 1)
+        //     {
+        //         middle++;
+        //     }
+        //     else if(a[middle]==2)
+        //     {
+        //         temp = a[middle];
+        //         a[middle] = a[high];
+        //         a[high] = temp;
+        //         high--;
+        //     }
+            
+            
+        // }
+        HashMap<Integer,Integer> s = new HashMap<>();
+        s.put(0,0);
+        s.put(1,0);
+        s.put(2,0);
+        for(int n:a)
+        {
+            s.put(n,s.get(n)+1);
+        }
+        int index = 0;
+        for(int i=0;i<3;i++)
+        { int freq = s.get(i);
+            for(int j=0;j<s.get(i);j++)
+            {
+                a[index++] = i;
+
+            }
+        }
+
         
 
 
