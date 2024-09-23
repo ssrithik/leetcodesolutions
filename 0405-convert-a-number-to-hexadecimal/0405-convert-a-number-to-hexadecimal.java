9199cf2fc8fc7db78a102c494ffbb14cf9f5cbdb
ssrithik <166838233+ssrithik@users.noreply.github.com>
2024-09-23 12:10:16 +0530

    Time: 0 ms (100%), Space: 40.6 MB (76.44%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -1,18 +1,39 @@
+// class Solution {
+//     public String toHex(int num) {
+//         // if(num == 0)
+//         // {
+//         //     return  "0";
+//         // }
+//         // char[] hexChars = "0123456789abcdef".toCharArray();
+//         // StringBuilder sb = new StringBuilder();
+//         // while(num!=0)
+//         // {
+//         //     int rem = num&15;
+//         //     sb.insert(0,hexChars[rem]);
+//         //     num >>>=4;
+//         // }
+//         // return sb.toString();
+        
+        
+//     }
+// }
 class Solution {
     public String toHex(int num) {
-        if(num == 0)
-        {
-            return  "0";
-        }
-        char[] hexChars = "0123456789abcdef".toCharArray();
-        StringBuilder sb = new StringBuilder();
-        while(num!=0)
-        {
-            int rem = num&15;
-            sb.insert(0,hexChars[rem]);
-            num >>>=4;
-        }
-        return sb.toString();
+        char[] hexas = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         
+        if (num == 0) return "0";
+        if (num > 0 && num <= 15) return Character.toString(hexas[num]);
+
+        long n = num;  // handle negative values
+        if (n < 0) n += (1L << 32);
+        
+        StringBuilder result = new StringBuilder();
+        
+        while (n > 0) {
+            result.insert(0, hexas[(int)(n % 16)]);
+            n /= 16;
+        }
+        
+        return result.toString();
     }
 }
