caa1b7407a8e90e99d9baa040006a234efc1872f
ssrithik <166838233+ssrithik@users.noreply.github.com>
2025-02-10 19:06:01 +0530

    Time: 0 ms (100%), Space: 104 MB (71.07%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.c
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.c
@@ -1,40 +1,21 @@
 
 int* rearrangeArray(int* nums, int numsSize, int* returnSize) {
-    int n = numsSize/2;
-    int* arr = (int*)malloc(n*sizeof(int));
-    int* arr1 = (int*)malloc(n*sizeof(int));
-    int i;
-    int index1=0;
-    int index2=0;
-    for(i=0;i<numsSize;i++)
-    {
-        if(nums[i] > 0)
-        {
-            arr[index1] = nums[i];
-            index1++;
-        }
-        else if(nums[i] < 0)
-        {
-            arr1[index2] = nums[i];
-            index2++;
-        }
-    }
-    *returnSize = numsSize;
-    index1=0;
-    index2=0;
+    int i=0,j=1;
     int* nums1 = (int*)malloc(numsSize*sizeof(int));
-    for(i=0;i<numsSize;i++)
+    *returnSize = numsSize;
+    int k;
+    for(k=0;k<numsSize;k++)
     {
-         if(i%2==0)
-         {
-             nums1[i] = arr[index1];
-             index1++;
-         }
-         else
-         {
-            nums1[i] = arr1[index2];
-            index2++;
-         }
+        if(nums[k]<0)
+        {
+            nums1[j] = nums[k];
+            j+=2; 
+        }
+        else
+        {
+            nums1[i] = nums[k];
+            i+=2;
+        }
     }
     return nums1;
 }
