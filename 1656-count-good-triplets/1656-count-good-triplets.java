1f8d2d1e749f625c19179e96e606a81ad6ef09c3
ssrithik <166838233+ssrithik@users.noreply.github.com>
2025-04-14 22:45:16 +0530

    Time: 13 ms (56.44%), Space: 41.3 MB (75.9%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.java
+++ 1656-count-good-triplets/1656-count-good-triplets.java
@@ -1,14 +1,14 @@
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
         int i,j,k;
-        int count=0;
+        int count = 0;
         for(i=0;i<arr.length;i++)
         {
             for(j=i+1;j<arr.length;j++)
             {
                 for(k=j+1;k<arr.length;k++)
                 {
-                    if(Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[i]-arr[k])<=c)
+                    if((Math.abs(arr[i]-arr[j])<=a) && (Math.abs(arr[j]-arr[k])<=b) && (Math.abs(arr[i]-arr[k]))<=c)
                     {
                         count++;
                     }
@@ -16,6 +16,5 @@ class Solution {
             }
         }
         return count;
-        
     }
 }
