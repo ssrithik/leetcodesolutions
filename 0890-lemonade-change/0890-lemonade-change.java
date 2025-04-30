7b341583beb1bef33214b2a805c35dd9d79f5c22
ssrithik <166838233+ssrithik@users.noreply.github.com>
2025-04-30 14:45:18 +0530

    Time: 2 ms (94.05%), Space: 56.2 MB (32.21%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,56 +1,99 @@
 class Solution {
     public boolean lemonadeChange(int[] arr) {
-        int i;
-        int[] change = new int[3];
-        for(i=0;i<arr.length;i++)
-        {
+        // int i;
+        // int[] change = new int[3];
+        // for(i=0;i<arr.length;i++)
+        // {
            
-           if(arr[i]==5) 
-           {change[0]++;
-           System.out.println(change[0]+" "+change[1]);
-           }
-           else if(arr[i]==10)
-           {
-            if(change[0] ==0)
-            {
-            return false;
-            }
-            change[1]++;
-            change[0]--;
-            System.out.println(change[0]+" "+change[1]);
+        //    if(arr[i]==5) 
+        //    {change[0]++;
+        //    System.out.println(change[0]+" "+change[1]);
+        //    }
+        //    else if(arr[i]==10)
+        //    {
+        //     if(change[0] ==0)
+        //     {
+        //     return false;
+        //     }
+        //     change[1]++;
+        //     change[0]--;
+        //     System.out.println(change[0]+" "+change[1]);
 
             
-           }
-           else if(arr[i]==20)
-           {
-            if(change[1]==0 && change[0] < 3)
-            {
-                    return false;
-            }
-            else if(change[1]>0 && change[0]==0)
-            {
-                return false;
-            }
-            else if(change[1] > 0 && change[0]>=1)
-            {
-                change[1]--;
-                change[0]--;
+        //    }
+        //    else if(arr[i]==20)
+        //    {
+        //     if(change[1]==0 && change[0] < 3)
+        //     {
+        //             return false;
+        //     }
+        //     else if(change[1]>0 && change[0]==0)
+        //     {
+        //         return false;
+        //     }
+        //     else if(change[1] > 0 && change[0]>=1)
+        //     {
+        //         change[1]--;
+        //         change[0]--;
               
-            }
-            else if(change[0] >=3)
-            {
-                change[0] = change[0]-3;
+        //     }
+        //     else if(change[0] >=3)
+        //     {
+        //         change[0] = change[0]-3;
                
-            }
-            System.out.println(change[0]+" "+change[1]);
-           }
-        }
-        System.out.println(change[0]+" "+change[1]);
-        if(change[0] <  0 || change[1] < 0)
+        //     }
+        //     System.out.println(change[0]+" "+change[1]);
+        //    }
+        // }
+        
+        // if(change[0] <  0 || change[1] < 0)
+        // {
+        //     return false;
+        // }
+        // else
+        // return true;
+        int i;
+        int five=0;
+        int ten=0;
+        for(i=0;i<arr.length;i++)
         {
-            return false;
+            int pay = arr[i];
+            if(pay == 5)
+            {
+                five++;
+            }
+            else if(pay == 10)
+            {
+                if(five <= 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    ten++;
+                    five--;
+                }
+            }
+            else if(pay == 20)
+            {
+                if((ten<1 && five <3) || (ten>0 && five<1))
+                {
+                    return false;
+                }
+                else
+                {
+                    if(ten>0)
+                    {
+                        ten--;
+                        five--;
+                    }
+                    else if(five >= 3)
+                    {
+                        five = five-3;
+                    }
+                }
+            }
         }
-        else
         return true;
     }
 }
