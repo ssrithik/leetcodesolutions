ebd4fa3317b8ede4083567c3ed8f8a1d00d3231e
ssrithik <166838233+ssrithik@users.noreply.github.com>
2025-06-11 14:53:36 +0530

    Time: 1099 ms (5.99%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -2,4 +2,4 @@ SELECT Employee.name,
 Bonus.bonus
 FROM Employee
 LEFT JOIN Bonus on Employee.empId = Bonus.empId
-WHERE Bonus.bonus < 1000 OR Bonus.bonus IS NULL
+WHERE bonus < 1000 OR Bonus IS NULL
