44ffc8dcf497d6f0bcfab8922be9afce26a799ea
ssrithik <166838233+ssrithik@users.noreply.github.com>
2025-10-30 20:20:40 +0530

    Time: 0 ms (100%), Space: 12.1 MB (94.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -1,16 +1,14 @@
 int removeDuplicates(int* nums, int numsSize) {
-    int i,j=1;
-    
+   int i,j=1;
+   for(i=1;i<numsSize;i++)
+   {
+     if(nums[i-1] != nums[i])
+     {
+        nums[j] = nums[i];
+        j++;
+     }
+
+   }
+   return j; 
    
-    for(i=1;i<numsSize;i++)
-    {
-        if(nums[i-1]!= nums[i])
-        {
-            nums[j] = nums[i];
-            j++;
-        }
-        
-    }
-    return j;
-    
 }
